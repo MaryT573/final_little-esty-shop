80593d6370a198603e864c56dbb85e0405ebe3be
Eli Sachs <easachs13@gmail.com>
2022-08-01 12:48:40 -0600

    Begin API structure

---
+++ app/services/github_service.rb
@@ -0,0 +1,9 @@
+class GithubService
+
+  def self.contributors
+    response = HTTParty.get 'https://api.github.com/repos/jusrez/little-esty-shop/contributors'
+
+    array = JSON.parse(response.body, symbolize_names: true).map{ |contributor| contributor[:login] }
+  end
+
+end
