8064e3837e9d9674be087ccd4bc7beb8a4372507
Justin Ramirez <jusrez528@gmail.com>
2022-08-01 14:35:15 -0400

    Add find invoice item method to controller and refactor update action in invoices controller

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,6 +1,7 @@
 class InvoicesController < ApplicationController
   before_action :find_merchant, only: [:index, :show, :update]
   before_action :find_invoice, only: [:show, :update]
+  before_action :find_invoice_item, only: [:update]
   
   def index
     @invoices = Invoice.find_with_merchant(@merchant)
@@ -10,8 +11,7 @@ class InvoicesController < ApplicationController
   end
 
   def update
-    invoice_item = InvoiceItem.find(params[:invoice_item_id])
-    invoice_item.update!(invoice_item_params)
+    @invoice_item.update!(invoice_item_params)
     redirect_to merchant_invoice_path(@merchant, @invoice)
   end
 
