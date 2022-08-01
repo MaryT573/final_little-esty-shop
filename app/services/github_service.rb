ba995935604dc0a7578c362d47dddc852d2eae06
Eli Sachs <easachs13@gmail.com>
2022-08-01 15:25:22 -0600

    Continue API structure

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -3,7 +3,7 @@ class GithubService
   def self.contributors
     response = HTTParty.get 'https://api.github.com/repos/jusrez/little-esty-shop/contributors'
 
-    array = JSON.parse(response.body, symbolize_names: true).map{ |contributor| contributor[:login] }
+    array = JSON.parse(response.body, symbolize_names: true).map{ |contributor| "#{contributor[:login]} - #{contributor[:contributions]} commits" }
   end
 
 end
