983ff75ef214b74e739b45fd6f37bf38bc73c8dd
MaryT573 <81441711+MaryT573@users.noreply.github.com>
2022-08-01 13:41:25 -0600

    advanced routing for admin merchants

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -1,6 +1,7 @@
 class Admin::MerchantsController < ApplicationController
   def index
     @merchants = Merchant.all
+    #require "pry"; binding.pry
     if params[:enable]
       merchant = Merchant.find(params[:id])
       merchant.update(status: "enabled")
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,13 @@ Rails.application.routes.draw do
   get '/', to: "welcome#index"
   get "/merchants/:id/dashboard", to: "merchants#show"
 
+  post '/merchants/:id/items', to: 'items#create'
+
+  get '/admin', to: 'admins#index'
+
+  get '/admin/merchants', to: 'admin/merchants#index'
+  patch '/admin/merchants', to: 'admin/merchants#index'
+  
   resources :merchants, except: [:show] do
     resources :items, only: [:index, :show, :edit, :update, :new]
     resources :invoices, only: [:index, :show, :update]
@@ -10,23 +17,6 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :invoices, only: [:index, :show, :update]
-    resources :merchants, only: [:index, :show, :new, :create, :edit, :update]
+    resources :merchants, only: [:show, :new, :create, :edit, :update]
   end
-
-  post '/merchants/:id/items', to: 'items#create'
-  get '/admin', to: 'admins#index'
-
-  # get '/admin/merchants/new', to: 'admin_merchants#new'
-  # post '/admin/merchants', to: 'admin_merchants#create'
-
-  # get '/admin/merchants/:id/edit', to: 'admin_merchants#edit'
-  # patch '/admin/merchants/:id', to: 'admin_merchants#update'
-
-  #get '/admin/merchants', to: 'admin_merchants#index'
-  #patch '/admin/merchants', to: 'admin_merchants#index'
-
-  # get '/admin/merchants/:id', to: 'admin_merchants#show'
-
-    # <%= button_to "Disable #{merchant.name}", "/admin/merchants?disable=#{params[:disable]}", method: :patch, local: true, params: {id: merchant.id}%> </p>
-
 end
