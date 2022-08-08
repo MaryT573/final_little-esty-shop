4227141b1302947142ffe124ab285a70ff73af24
MaryT573 <81441711+MaryT573@users.noreply.github.com>
2022-08-08 13:34:54 -0600

    added validation tests for bulkdiscount

--- spec/models/bulkdiscount_spec.rb
+++ spec/models/bulkdiscount_spec.rb
@@ -4,4 +4,9 @@ RSpec.describe Bulkdiscount, type: :model do
   describe 'relationships' do
     it { should belong_to(:merchant) }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of(:threshold) }
+    it { should validate_presence_of(:discount) }
+  end
 end
